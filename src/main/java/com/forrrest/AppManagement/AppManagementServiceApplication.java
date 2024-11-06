555bf255a6a7d545d20ffda0c4d7990c65ef092f
beomkyu choi <beomkyuchoi069@gmail.com>
2024-11-06 09:55:17 +0900

    First commit : Project setup

---
+++ src/main/java/com/forrrest/AppManagement/AppManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.forrrest.AppManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forrrest/AppManagement/AppManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forrrest.AppManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
